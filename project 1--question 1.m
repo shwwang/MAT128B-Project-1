54248caa91d4d805be297d1145b504e57fdd5b71
qyyuan <36680025+qyyuan@users.noreply.github.com>
2018-02-20 13:46:11 -0800

    Add files via upload

---
+++ project 1--question 1.m
@@ -0,0 +1,49 @@
+phi = inline ('z^2-1.25')
+
+fixpt1 = (1+sqrt(6))/2;
+fixpt2 = (1-sqrt(6))/2;
+colormap([1 0 0; 1 1 1]);
+M=2*ones(141,361);
+
+for j=1:141
+    y=-.7 + (j-1)*.01;
+  for i = 1:361
+     x= -1.8 +(i-1)*.01;
+     z= x+ 1i*y;
+     zk=z;
+     iflag1=0;
+     iflag2=0;
+     kount=0;
+   
+       while kount<100 && abs(zk)<2 && iflag1<5 && iflag2<5
+       kount=kount+1;
+       zk = phi (zk);
+    
+       err1= abs(zk-fixpt1);
+       if err1 < 1.e-6
+           iflag1 = iflag1 +1;
+       else
+            iflag1 = 0;
+       end
+    
+       err2= abs(zk-fixpt2);
+       if err2 < 1.e-6
+         iflag2 = iflag2 +1;
+       else 
+        iflag2 = 0;
+       end
+    
+       end 
+
+       if iflag1>= 5 || iflag2>=5 || kount >=100
+         M(j,i)=1;
+       end
+    
+  end
+end
+
+image ([-1.8 1.8], [-.7 .7], M),
+axis xy
+
+
+
